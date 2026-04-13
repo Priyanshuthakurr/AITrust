97cf61bfbfa3b916e143cf6f73985b19b9dedc33
PRIYANSHU THAKUR <priyanshut740@gmail.com>
2026-04-13 13:13:30 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 # import cv2
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 
 # -----------------------------
 # PAGE CONFIG
