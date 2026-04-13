57bb54906cdf09e7efcacb1bc737ee9801d7609d
PRIYANSHU THAKUR <priyanshut740@gmail.com>
2026-04-13 13:23:53 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pickle
 import numpy as np
-import cv2
+# import cv2
 from PIL import Image
 import pytesseract
 import pdfplumber
 import tempfile
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
